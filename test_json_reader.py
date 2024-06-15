f00ecbc5fa9fc5d147021fe7eae81395be01bdbc
Michael Hensley <29671149+ZRexshima@users.noreply.github.com>
2024-06-15 22:07:48 +0900

    adds tests for write function

--- test_json_reader.py
+++ test_json_reader.py
@@ -12,3 +12,14 @@ def test_get_saved_json():
 
     test_list: list[dict] = get_saved_json(test_file)
     assert test_list[0] == {'date': '2024-06-15', 'rate': 153.3}
+
+
+def test_write_json():
+    """Tests saving json to file"""
+    data: list[dict] = [{'date': '2024-06-15', 'rate': 153.3}]
+    test_file: Path = Path.cwd() / 'test.json'
+    json_payload = json.dumps(data, indent=4)
+    test_file.write_text(json_payload)
+
+    file_data = test_file.read_text()
+    assert file_data == json_payload
